1dc8b4a0a2131714271776f7148f1aae298d6d35
Herry Prasetyo <herry081288@gmail.com>
2022-05-22 12:22:34 +0700

    okDoneResponseHeader

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const http = require('http');
 
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
-
-
-    response.statusCode = 200;
-
-
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('X-Powered-By', 'Node.js');
 
     const { method, url } = request;
 
     if (url === '/') {
         if (method === 'GET') {
-
             response.statusCode = 200;
             response.end('<h1>Ini adalah homepage</h1>');
         } else {
@@ -22,14 +17,6 @@ const requestListener = (request, response) => {
     } else if (url === '/about') {
         if (method === 'GET') {
             response.statusCode = 200;
-
-            response.end('<h1>Ini adalah homepage</h1>');
-        } else {
-            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
-        }
-    } else if (url === '/about') {
-        if (method === 'GET') {
-
             response.end('<h1>Halo! Ini adalah halaman about</h1>')
         } else if (method === 'POST') {
             let body = [];
@@ -41,7 +28,6 @@ const requestListener = (request, response) => {
             request.on('end', () => {
                 body = Buffer.concat(body).toString();
                 const { name } = JSON.parse(body);
-
                 response.statusCode = 200;
                 response.end('<h1>Halo, ${name}! Ini adalah halaman about</h1>');
             });
@@ -52,33 +38,6 @@ const requestListener = (request, response) => {
     } else {
         response.statusCode = 404;
         response.end('<h1>Halaman tidak ditemukan!</h1>');
-
-        response.end('<h1>Halo, ${name}! Ini adalah halaman about</h1>');
-    } else {
-    response.end('<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>');
-        
-    }else {
-    response.end('<h1>Halaman tidak ditemukan!</h1>');
-    
-
-    const { method } = request;
-
-    if (method === 'GET') {
-        response.end('<h1>Hello!</h1>');
-    }
-
-    if (method === 'POST') {
-        response.end('<h1>Hai!</h1>');
-    }
-
-    if (method === 'PUT') {
-        response.end('<h1>Bonjour!</h1>');
-    }
-
-    if (method === 'DELETE') {
-        response.end('<h1>Salam!</h1>');
-
-
     }
 };
 
